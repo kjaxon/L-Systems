cc0d6d349e39a8e720a250e96a6081a5df9528f1
LOGIN <larissachen13@gmail.com>
2017-11-20 20:39:31 -0500

    finished

--- lsystem.js
+++ lsystem.js
@@ -15,13 +15,16 @@ var isSymbol = function(character ){
         && character != "[" && character != "]";
 }
 
-var Lsystem = function(axiom, rules, angle, iterations) {
+var Lsystem = function(axiom, rules, angle, iterations, offsetx, offsety) {
     this.axiom = axiom;
     this.rules = rules;
     this.angle =  angle;
     this.length = 2;
     this.lstring = "";
     this.iterations = iterations;
+    this.randomAngle = false;
+    this.offsetx = offsetx;
+    this.offsety = offsety;
 }
 
 Lsystem.prototype.generateString = function() {
@@ -53,21 +56,25 @@ Lsystem.prototype.getStochasticRule = function(symbol) {
     return this.rules[symbol][index];
 }
 
-Lsystem.prototype.rulesToStrings = function() {
+Lsystem.prototype.copy = function() {
+    return new Lsystem(this.axiom, this.rules, this.angle, this.iterations, this.offsetx, this.offsety);
+}
+
+function rulesToString(rules) {
     var lines = [];
     var arrow = " --> ";
-    for (var symbol in this.rules) {
-        if (this.rules[symbol] instanceof Array) {
-            for (var i = 0; i < this.rules[symbol].length; i++) {
+    for (var symbol in rules) {
+        if (rules[symbol] instanceof Array) {
+            for (var i = 0; i < rules[symbol].length; i++) {
                 var line = symbol;
                 line += arrow;
-                line += this.rules[symbol][i];
+                line += rules[symbol][i];
                 lines.push(line);
             }
         } else {
             var line = symbol;
             line += arrow;
-            line += this.rules[symbol];
+            line += rules[symbol];
             lines.push(line);
         }
 
@@ -77,13 +84,14 @@ Lsystem.prototype.rulesToStrings = function() {
 
 Lsystem.prototype.draw = function(canvas, w, h) {
     var context = canvas.getContext('2d');
-    var turtle = new Turtle(w/2, h/2, 0,  context);
+    var turtle = new Turtle(w + this.offsetx, h + this.offsety, 0,  context);
     for (var i = 0; i < this.lstring.length; i++) {
         var letter = this.lstring.charAt(i);
+        var angle = this.randomAngle ? Math.floor(Math.random() * 360) : this.angle;
         if (letter == "+") {
-            turtle.turnLeft(this.angle);
+            turtle.turnLeft(angle);
         } else if (letter == "-") {
-            turtle.turnRight(this.angle);
+            turtle.turnRight(angle);
         } else if (letter == "[") {
             turtle.push();
         } else if (letter == "]") {
@@ -138,6 +146,7 @@ Lsystem.prototype.setIterations = function(n){
 
 var LsystemSetup = function(canvas) {
     this.lsystem = null;
+    this.instances = [];
 }
 
 LsystemSetup.prototype.setLsystem = function(type) {
@@ -153,47 +162,80 @@ LsystemSetup.prototype.setLsystem = function(type) {
             model = tree1;
         } else if (type == "tree2") {
             model = tree2;
+        } else if (type == "tree3") {
+            model = tree3;
+        } else if (type == "tree4") {
+            model = tree4;
         } else if (type == "sb") {
             model = sb;
         } else if (type == "stoch1") {
             model = stoch1;
         }
-        this.lsystem = new Lsystem(model.axiom, model.rules, model.angle, model.n);
+        this.lsystem = new Lsystem(model.axiom, model.rules, model.angle, model.n, model.offsetx, model.offsety);
         this.lsystem.generateString();
     }
 }
 
 LsystemSetup.prototype.setCustom  = function () {
-    this.lsystem = new Lsystem(axiom, dict, angle, n);
+    this.lsystem = new Lsystem(axiom, dict, angle, n, 0, 0);
 }
 
 LsystemSetup.prototype.reset = function() {
     dict = {};
     axiom = "";
     this.lsystem = null;
+    this.instances = [];
 }
 
 LsystemSetup.prototype.render = function(canvas, w, h) {
 
     var context = canvas.getContext('2d');
     clear(context, w, h);
-    if (this.lsystem != null ) {
-        this.lsystem.draw(canvas, w, h);
-        document.querySelector('.display-axiom').innerHTML = 'Axiom: ' + this.lsystem.axiom;
-        document.querySelector('.display-rules').innerHTML = 'Rules: ';
-        var lines = this.lsystem.rulesToStrings();
-        for (var i = 0; i < lines.length; i++) {
-            document.querySelector('.display-rules').innerHTML += "<div class='tab'> " + lines[i] + "</div>" ;
+    if (this.lsystem != null) {
+        var noOfCopies = document.getElementById("instances").value;
+        if (noOfCopies > 1) {
+            if (this.instances.length == 0) {
+                this.instances.push(this.lsystem);
+            }
+            for (var i = this.instances.length; i < noOfCopies; i++) {
+                var copy = this.lsystem.copy();
+                copy.generateString();
+                this.instances.push(copy);
+            }
+            var incrementx = w/(this.instances.length + 1);
+            for (var i = 0; i < this.instances.length; i++) {
+                this.instances[i].draw(canvas, (i + 1) * incrementx, h/2);
+            }
+        } else {
+            this.instances = [];
+            this.lsystem.draw(canvas, w/2, h/2);
         }
-        document.querySelector('.display-n').innerHTML = 'Iterations: ' + this.lsystem.iterations;
-        document.querySelector('.display-length').innerHTML = 'Length: ' + this.lsystem.length;
-        document.querySelector('.display-angle').innerHTML = 'Angle: ' + this.lsystem.angle;
     }
+    var axiomString = this.lsystem != null ? this.lsystem.axiom : axiom;
+    var nString = this.lsystem != null ? this.lsystem.iterations : 0;
+    var lengthString = this.lsystem != null ? this.lsystem.length : 0;
+    var angleString = this.lsystem != null ? this.lsystem.angle : 0;
+    var lines = this.lsystem != null ? rulesToString(this.lsystem.rules) : rulesToString(dict);
+    document.querySelector('.display-axiom').innerHTML = 'Axiom: ' + axiomString;
+    document.querySelector('.display-rules').innerHTML = 'Rules: '
+    for (var i = 0; i < lines.length; i++) {
+        document.querySelector('.display-rules').innerHTML += "<div class='tab'> " + lines[i] + "</div>" ;
+    }
+
+    document.querySelector('.display-n').innerHTML = 'Iterations: ' + nString;
+    document.querySelector('.display-length').innerHTML = 'Length: ' + lengthString;
+    document.querySelector('.display-angle').innerHTML = 'Angle: ' + angleString;
 }
 
 LsystemSetup.prototype.setAngle = function(angle) {
     if (this.lsystem != null) {
         this.lsystem.setAngle(angle);
+    }
+}
+
+LsystemSetup.prototype.setRandomAngle = function(cb) {
+    if (this.lsystem != null) {
+        this.lsystem.randomAngle = cb.checked;
     }
 }
 
